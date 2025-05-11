82fe83fcae54ee530e3bdfd27ab00a44da4c0560
Terencetjt26 <10597@holbertonstudents.com>
2025-05-11 08:23:16 +0000

    Add fizzbuzz function to print numbers 1 to 100 with FizzBuzz logic

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import random
 
-number = random.randint(-10, 10)
-
-	if number > 0:
-		print(f"{number} is positive")
-	elif number == 0:
-		print(f"{number} is zero")
-	else:
-		print(f"{number} is negative")
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(i, end=" ")
