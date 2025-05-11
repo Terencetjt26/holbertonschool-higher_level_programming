3fa99ad4dee5d8e01aa1736d83909276e06c6649
Terencetjt26 <10597@holbertonstudents.com>
2025-05-11 09:54:42 +0000

    Task 4: List names from hidden_4 module

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
-import hidden_4
-
+#!/usr/bin/python3
 if __name__ == "__main__":
-    names = dir(hidden_4)
-    for name in names:
+    import hidden_4
+
+    for name in sorted(dir(hidden_4)):
         if not name.startswith("__"):
             print(name)
 
