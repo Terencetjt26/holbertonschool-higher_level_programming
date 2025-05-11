45e6f36c21a091e5307a3665e0ebaad6589a58fb
Terencetjt26 <10597@holbertonstudents.com>
2025-05-11 10:06:02 +0000

    Add 4-hidden_discovery.py for module name discovery task

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,7 @@
 if __name__ == "__main__":
     import hidden_4
 
-    for name in sorted(dir(hidden_4)):
+    names = dir(hidden_4)
+    for name in sorted(names):
         if not name.startswith("__"):
             print(name)
-
-
