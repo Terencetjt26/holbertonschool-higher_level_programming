c6b320057a1d18047c41e248a233965fc33bfa33
Terencetjt26 <10597@holbertonstudents.com>
2025-05-14 20:57:47 +0000

    Add function to print a matrix of integers

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for index, num in enumerate(row):
+            if index != 0:
+                print(" ", end="")
+            print("{:d}".format(num), end="")
+        print("")
