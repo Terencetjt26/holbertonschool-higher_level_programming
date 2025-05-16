aed2dae9916e325fcbe423f2d85b807975f2126c
Terencetjt26 <10597@holbertonstudents.com>
2025-05-16 22:37:04 +0000

    Fix: handle float overflow and ensure full compliance with test cases

--- python-test_driven_development/0-main.py
+++ python-test_driven_development/0-main.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 add_integer = __import__('0-add_integer').add_integer
 
-print(add_integer(1, 2))
-print(add_integer(100, -2))
-print(add_integer(2))
-print(add_integer(100.3, -2))
+print(add_integer(1, 2))        # 3
+print(add_integer(100, -2))     # 98
+print(add_integer(2))           # 100 (2 + default 98)
+print(add_integer(100.3, -2))   # 98 (int cast)
 try:
     print(add_integer(4, "School"))
 except Exception as e:
-    print(e)
+    print(e)                   # b must be an integer
 try:
     print(add_integer(None))
 except Exception as e:
-    print(e)
+    print(e)                   # a must be an integer
