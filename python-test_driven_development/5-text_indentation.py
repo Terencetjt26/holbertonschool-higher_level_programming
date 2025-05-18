429144889d01c37914561348d64f313b404e9383
Terencetjt26 <10597@holbertonstudents.com>
2025-05-18 09:25:43 +0000

    Add text_indentation function to print with formatting after ., ?, :

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -4,16 +4,28 @@ Module 5-text_indentation
 Defines a function that prints text with 2 new lines after '.', '?' and ':'
 """
 
+
 def text_indentation(text):
+    """
+    Prints text with two new lines after each '.', '?', and ':'.
+
+    Args:
+        text (str): The text to be processed
+
+    Raises:
+        TypeError: if text is not a string
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
     i = 0
     while i < len(text):
         print(text[i], end="")
-        if text[i] in ".?:":
-            print("\n")  # Deux nouvelles lignes
+        if text[i] in ".:?":
+            print("\n")
             i += 1
-            # Ne pas supprimer les espaces ici, laisser le texte tel quel
-        else:
-            i += 1
+            # Skip following spaces
+            while i < len(text) and text[i] == " ":
+                i += 1
+            continue
+        i += 1
