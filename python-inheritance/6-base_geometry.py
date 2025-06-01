cc8d90221b23108d084d938989d8c0ddd59d7e33
Terencetjt26 <10597@holbertonstudents.com>
2025-06-01 17:27:58 +0000

    Add area() method to BaseGeometry that raises NotImplemented exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Class BaseGeometry with an area method not implemented."""
+
+
+class BaseGeometry:
+    def area(self):
+        """Raises an exception because area is not implemented."""
+        raise Exception("area() is not implemented")
