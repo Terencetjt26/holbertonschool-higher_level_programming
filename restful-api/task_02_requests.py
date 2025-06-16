1c4a0e58763c59f94b12f4ab18930e82c732d482
Terencetjt26 <10597@holbertonstudents.com>
2025-06-16 09:29:42 +0000

    Add Fetch and process API data: print post titles and save to CSV

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+import requests
+import csv
+
+def fetch_and_print_posts():
+    """Fetch posts and print status code and titles"""
+    url = "https://jsonplaceholder.typicode.com/posts"
+
+    try:
+        response = requests.get(url)
+        print(f"Status Code: {response.status_code}")
+        response.raise_for_status()
+
+        posts = response.json()
+        for post in posts:
+            print(post.get("title"))
+
+    except requests.RequestException:
+        print("Fetch fail")
+
+
+def fetch_and_save_posts():
+    """Fetch posts and save selected fields into a CSV"""
+    url = "https://jsonplaceholder.typicode.com/posts"
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        posts = response.json()
+        data = [{"id": post["id"], "title": post["title"], "body": post["body"]} for post in posts]
+
+        with open("posts.csv", "w", newline='', encoding='utf-8') as csvfile:
+            fieldnames = ["id", "title", "body"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(data)
+
+    except requests.RequestException:
+        print("Fetch fail")
