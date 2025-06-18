f30f125d2fd8648ec46285a900ef85e3400b2f69
Terencetjt26 <10597@holbertonstudents.com>
2025-06-18 07:27:09 +0000

    Add fetch posts from API and process them (print titles and export to CSV)

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,42 +1,39 @@
 #!/usr/bin/python3
-import requests
+"Module for make requests to a RESTful api"
+
 import csv
+import requests
+
 
 def fetch_and_print_posts():
-    """Fetch posts and print status code and titles"""
+    """
+    Fetch posts from JSONPlaceholder and print titles
+    """
     url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    print(f"Status Code: {response.status_code}")
 
-    try:
-        response = requests.get(url)
-        print(f"Status Code: {response.status_code}")
-        response.raise_for_status()
-
+    if response.status_code == 200:
         posts = response.json()
         for post in posts:
-            print(post.get("title"))
-
-    except requests.RequestException:
-        print("Fetch fail")
+            print(post['title'])
 
 
 def fetch_and_save_posts():
-    """Fetch posts and save selected fields into a CSV"""
+    """
+    Fetch posts from JSONPlaceholder and save to CSV
+    """
     url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
 
-    try:
-        response = requests.get(url)
-		if response.status_code != 200:
-			print("Fetch fail")
-			return
-
+    if response.status_code == 200:
         posts = response.json()
-        data = [{"id": post["id"], "title": post["title"], "body": post["body"]} for post in posts]
+        data_to_write = [
+            {'id': post['id'], 'title': post['title'], 'body': post['body']}
+            for post in posts
+        ]
 
-        with open("posts.csv", "w", newline='', encoding='utf-8') as csvfile:
-            fieldnames = ["id", "title", "body"]
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        with open('posts.csv', 'w', newline='', encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
             writer.writeheader()
-            writer.writerows(data)
-
-    except requests.RequestException:
-        print("Fetch fail")
+            writer.writerows(data_to_write)
